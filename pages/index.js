fc11d16d30381f0a95771b5305d008b0ce07a010
Gustavo Feitosa <gustavofeitosaaa@gmail.com>
2023-04-01 12:22:47 -0300

    mais nova pagina

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react"
 
 function Home() {
-    return <div>
-        <h1>Quantos beijinhos você quer?</h1>
+    return <div style={{background: "red", borderRadius: 30, padding: 10,}}>
+        <h1>Quantos beijinhos você quer? hein?</h1>
         <Contador/> 
     </div>
 }
@@ -15,9 +15,21 @@ function Contador(){
     }
 
     return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionaContador}>Adicionar</button>
+        <div style={{}}>
+            <div>
+                <h1>{contador}</h1>
+                {contador <= 10
+                ?
+                <h3>Poxa só {contador}? :/</h3>
+                :
+                <h3>Agora sim começou a ficar interessante....</h3>
+                
+                }
+            </div>
+
+            <button style={{width: "100%", padding: 5, color: "purple", fontWeight: "bolder", fontSize: 20, borderRadius: 10, borderColor: "purple"}} onClick={adicionaContador}>Adicionar ++++</button>
+
+            <button style={{width: "100%", padding: 5, color: "purple", fontWeight: "bolder", fontSize: 20, borderRadius: 10, borderColor: "purple", marginTop: 10, marginBottom: 10}} onClick={() => {alert ("Você quer diminuir porquê? Você não gosta de mim? T-T")}}>Diminuir ---- :/</button>
         </div>
     )
 }
