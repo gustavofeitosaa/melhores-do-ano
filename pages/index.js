b94f27843200d3aa4baa28aae63aff31a788a3e4
Gustavo Feitosa <gustavofeitosaaa@gmail.com>
2023-04-01 11:46:43 -0300

    olha la

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
-// import React, { useState } from "react"
+import React, { useState } from "react"
 
 function Home() {
     return <div>
-        <h1>Home</h1>
-        {/* <Contador/>  */}
+        <h1>Quantos beijinhos você quer?</h1>
+        <Contador/> 
     </div>
 }
 
-// function Contador(){
-//     const [contador, setContador] = useState(1);
+function Contador(){
+    const [contador, setContador] = useState(1);
 
-//     function adicionaContador(){
-//         setContador(contador + 1);
-//     }
+    function adicionaContador(){
+        setContador(contador + 1);
+    }
 
-//     return (
-//         <div>
-//             <div>{contador}</div>
-//             <button onClick={adicionaContador}>Adiciona</button>
-//         </div>
-//     )
-// }
+    return (
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionaContador}>Adicionar</button>
+        </div>
+    )
+}
 
 export default Home
